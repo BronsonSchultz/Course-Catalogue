9d2243101c0f8b073e9167a5050cb78535b536ab
Bronson Schultz <58102397+BronsonSchultz@users.noreply.github.com>
2020-03-25 14:33:30 -0600

    cleaned up faq page and added second class stub to catalogue

--- webServer/complete/src/main/java/webApp/WebController.java
+++ webServer/complete/src/main/java/webApp/WebController.java
@@ -2,9 +2,7 @@ package webApp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -33,10 +31,8 @@ public class WebController implements WebMvcConfigurer {
 
 
 
-
-	// catalogue
-	@GetMapping("/catalogue")
-	public String searchForm(Model model) {
+	@RequestMapping(value="/catalogue", method = {RequestMethod.GET, RequestMethod.POST})
+	public String createCourses(Model model, @ModelAttribute("searchForm") SearchForm searchForm){
 		model.addAttribute("searchForm", new SearchForm());
 
 		SearchResults s = new SearchResults();
@@ -56,21 +52,22 @@ public class WebController implements WebMvcConfigurer {
 		model.addAttribute("s", s);
 
 		return "catalogue";
-
 	}
 
-
-
-
-	@PostMapping("/catalogue")
-	public String searchSubmit(@ModelAttribute("searchForm") SearchForm searchForm) {
-		return "catalogue";
-	}
 
 
 	// faq
 	@GetMapping("/faq")
-	public String faq(){return "faq";}
+	public String faq(Model model){
+		model.addAttribute("bugReportForm", new BugReportForm());
+		return "faq";
+	}
+
+	@PostMapping("/faq")
+	public String sendBug(@ModelAttribute("bugReportForm") BugReportForm bugReportForm){
+		System.out.println(bugReportForm.getDescription());
+		return "faq";
+	}
 
 	// schedule
 	@GetMapping("/schedule")
