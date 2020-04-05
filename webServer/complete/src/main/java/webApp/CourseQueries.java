4b05b2b729f75d7ddcc1cb40df313fc003d79618
Bronson Schultz <58102397+BronsonSchultz@users.noreply.github.com>
2020-04-04 18:39:40 -0600

    wrote query for getting a user's favourite courses and testing that method

--- webServer/complete/src/main/java/webApp/CourseQueries.java
+++ webServer/complete/src/main/java/webApp/CourseQueries.java
@@ -26,27 +26,38 @@ public class CourseQueries {
         //tests
         CourseQueries querier = new CourseQueries();
 
+        //get courses for search tests /////////////
         // both fields present
+        System.out.println("Expecting all of the CMPT courses to be at the 400-level");
         ArrayList<HashMap<String, String>> l = querier.getCoursesFromDB("CMPT", "4");
         System.out.println(l);
 
 
         // just subject present
+        System.out.println("Expecting all of the CMPT courses at any level");
         l = querier.getCoursesFromDB("CMPT", "");
         System.out.println(l);
 
         // just course code present
+        System.out.println("Expecting all of the 100-level courses from any subject");
         l = querier.getCoursesFromDB("", "1");
         System.out.println(l);
 
-        // jsonify each map in the arrayList
-        JSONObject[] js = querier.jsonifyList(l);
 
+        // jsonify each map in the arrayList
+        System.out.println("Expecting array of JSON objects");
+        JSONObject[] js = querier.jsonifyList(l);
         System.out.println(Arrays.toString(js));
 
-        //garbage
-        System.out.println("'openGate(\'' +${gate.gateIp} +'\',\''+${gate.gatePort}+'\')'");
-        System.out.println("'javascript:openCourseSideBar(\'' +${course.getString(SubjectCode)} +'\',\''+${course.getString(CourseCode)}+'\',\''+${course.getString(CourseName)}+'\',\''+${course.getString(Description)}+'\')'");
+
+        // get favourite courses test ///////////
+        System.out.println("Expecting all of user 1's favourited courses");
+        l = querier.getUserFavCourses("1");
+        System.out.println(l);
+
+
+
+
     }
 
     /**
@@ -59,7 +70,7 @@ public class CourseQueries {
     public ArrayList<HashMap<String, String>> getCoursesFromDB(String subjectCode, String courseCode) throws SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
-        ArrayList<HashMap<String, String>> stringCourses = new ArrayList<>();
+        ArrayList<HashMap<String, String>> mappedCourses = new ArrayList<>();
 
         try {
             conn = db.getConn();
@@ -97,7 +108,7 @@ public class CourseQueries {
             // get results
             ResultSet rs = stmt.executeQuery();
 
-            // print query results
+
             while (rs.next()) {
                 HashMap<String, String> course = new HashMap<>();
                 course.put("SubjectCode", rs.getString("SubjectCode"));
@@ -105,11 +116,12 @@ public class CourseQueries {
                 course.put("CourseName", rs.getString("CourseName"));
                 course.put("Description", rs.getString("Description"));
 
-                stringCourses.add(course);
+                mappedCourses.add(course);
             }
 
         } catch (SQLException e) {
             System.out.println(e);
+
 
         } finally {
             // close statement and connection objects to conserve DBMS resources
@@ -121,9 +133,60 @@ public class CourseQueries {
                 conn.close();
             }
         }
-        return stringCourses;
+        return mappedCourses;
     }
 
+    /**
+     * query the database for the favourited courses of a user
+     * @param userID the id of the logged in user
+     * @return an array list of courses as hashMaps
+     * @throws SQLException if a connection to the db cannot be formed
+     */
+    public ArrayList<HashMap<String, String>> getUserFavCourses(String userID) throws SQLException{
+        String sql = "SELECT fl.SubjectCode, fl.CourseCode, fl.UserID, CourseName, Description FROM (FavouriteList fl" +
+                " JOIN Courses " +
+                "ON fl.SubjectCode = Courses.SubjectCode AND fl.CourseCode = Courses.CourseCode)" +
+                "WHERE fl.UserID = ?";
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ArrayList<HashMap<String, String>> mappedCourses = new ArrayList<>();
+
+        try {
+            conn = db.getConn();
+
+            // create statement with sql template
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, userID);
+
+            // get results
+            ResultSet rs = stmt.executeQuery();
+
+
+            while (rs.next()) {
+                HashMap<String, String> course = new HashMap<>();
+                course.put("SubjectCode", rs.getString("SubjectCode"));
+                course.put("CourseCode", rs.getString("CourseCode"));
+                course.put("CourseName", rs.getString("CourseName"));
+                course.put("Description", rs.getString("Description"));
+
+                mappedCourses.add(course);
+            }
+
+        } catch (SQLException e){
+            System.out.println(e);
+        } finally {
+        // close statement and connection objects to conserve DBMS resources
+        if (stmt != null) {
+            stmt.close();
+        }
+
+        if (conn != null) {
+            conn.close();
+        }
+    }
+
+        return mappedCourses;
+    }
 
     /**
      * create an array of json objects from an array list
