7d60b4586a619b813f3f65541ff79d990a18e39f
Chandra Lyons (cml422) <chandra.lyons@usask.ca>
2020-04-07 14:19:24 -0600

    Delete test_reset.sql

--- webServer/complete/src/main/query/db_upkeep/test_reset.sql
+++
@@ -1,9 +0,0 @@
-
-.mode csv
-.import CSVs/Courses_data.csv Courses
-.import CSVs/non-CMPTDegree_Courses_data.csv Courses
-.import CSVs/Degrees_data.csv Degrees
-.import CSVs/CreditConflicts_data.csv CreditConflicts
-.import CSVs/DegreeRequirements_data.csv DegreeRequirements
-.import CSVs/Prerequisites_data.csv Prerequisites
-.mode list
