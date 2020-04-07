b0436adb803e2be927387c70ce40ac7368ed895e
Chandra Lyons (cml422) <chandra.lyons@usask.ca>
2020-04-07 12:18:19 -0600

    Upload New reset_FillData2 File

---
+++ reset_FillData2.sql
@@ -0,0 +1,5 @@
+.mode csv
+.import CSVs/CreditConflicts_data.csv CreditConflicts
+.import CSVs/DegreeRequirements_data.csv DegreeRequirements
+.import CSVs/Prerequisistes_data.csv Prerequisites
+.mode list
