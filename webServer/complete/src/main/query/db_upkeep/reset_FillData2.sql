d9ea3bb98e2e97aa02cd2b4fda3955f57db4daa9
Chandra Lyons (cml422) <chandra.lyons@usask.ca>
2020-04-07 14:18:51 -0600

    Delete reset_FillData2.sql

--- webServer/complete/src/main/query/db_upkeep/reset_FillData2.sql
+++
@@ -1,5 +0,0 @@
-.mode csv
-.import CSVs/CreditConflicts_data.csv CreditConflicts
-.import CSVs/DegreeRequirements_data.csv DegreeRequirements
-.import CSVs/Prerequisistes_data.csv Prerequisites
-.mode list
